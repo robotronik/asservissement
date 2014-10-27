a9814d7916f9155cf4aa9b25e7918ec965c6dd73
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2014-10-27 22:45:23 +0100

    utilisation de set_position sans convertion

--- communication.c
+++ communication.c
@@ -32,5 +32,5 @@ void send_position_atteinte()
 
 void send_position_xbee()
 {
-	set_position(get_x_actuel(), get_y_actuel(),get_theta_actuel()/1000*180/3.14159); //à virer
+	set_position(get_x_actuel(), get_y_actuel(),get_theta_actuel()); //à virer
 }
