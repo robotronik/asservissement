45621db4e6903f681ddd1e99e12d4b53fd49d911
Club Robotronik <robotronik.phelma@gmail.com>
2014-11-18 15:10:33 +0100

    fct pause en double + déplacement fct débug

--- hardware_PIC.c
+++ hardware_PIC.c
@@ -7,6 +7,20 @@
 
 int doitAttendre;
 
+/*----------------------------------------------------------------*
+ * fonctions de debug                                             *
+ *----------------------------------------------------------------*/
+
+void allumer_del(void)
+{
+    _RB5 = 0; // nouvelle carte
+}
+
+void eteindre_del(void)
+{
+    _RB5 = 1; // nouvelle carte
+}
+
 void pause_ms(unsigned short n)
 {
     unsigned short i,j;
@@ -359,37 +373,6 @@ void __attribute__((__interrupt__, no_auto_psv)) _T1Interrupt(void)
     doitAttendre = 0;
     IFS0bits.T1IF = 0;
     TMR1 = 0;
-}
-
-/*----------------------------------------------------------------*
- * fonctions de debug                                             *
- *----------------------------------------------------------------*/
-
-void allumer_del(void)
-{
-	_RB5 = 0; // nouvelle carte
-}
-
-void eteindre_del(void)
-{
-	_RB5 = 1; // nouvelle carte
-}
-
-void pause_ms(unsigned short n)
-{
-	unsigned short i,j;
-	for(j=0; j<n; j++)
-	{
-     	for(i=0; i<4000; i++)
-			Nop();
-	}
-}
-
-void pause_s(unsigned short n)
-{
-	unsigned short i;
-	for(i=0; i<n; i++)
-		pause_ms(1000);
 }
 
 /*---------------------------------------------------------------------------*
