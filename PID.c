c66e23ac18ecd61f18b1c31c5a4877ec3d8122fc
PC du club <postmaster@robotronik.org>
2015-05-04 13:00:31 +0200

    ajout de la posibilité de modifier à chaud les coeff P et D des PID linéiques et angulaires

--- PID.c
+++ PID.c
@@ -10,16 +10,21 @@
 #endif
 #include "asser.h"
 
+static int kp_delta=KP_DELTA;
+static int kd_delta=KD_DELTA;
+static int kp_alpha=KP_ALPHA;
+static int kd_alpha=KD_ALPHA;
+
 long int PID_lineique(s_erreur erreur_delta)
 {
 	long int commande;
 
 	//composante proportionelle
-	commande=(long int) KP_DELTA*erreur_delta.actuelle;
+	commande=(long int) kp_delta*erreur_delta.actuelle;
 	//composante intégrale
 	commande+=(long int) KI_DELTA*erreur_delta.sum;
 	//composante dérivée
-	commande+=(long int) KD_DELTA*(erreur_delta.actuelle-erreur_delta.preced);
+	commande+=(long int) kd_delta*(erreur_delta.actuelle-erreur_delta.preced);
 
 	return commande;
 }
@@ -29,11 +34,31 @@ long int PID_angulaire(s_erreur erreur_alpha)
 	long int commande;
 	
 	//composante proportionelle
-	commande=(long int) KP_ALPHA*erreur_alpha.actuelle;
+	commande=(long int) kp_alpha*erreur_alpha.actuelle;
 	//composante intégrale
 	commande+=(long int) KI_ALPHA*erreur_alpha.sum;
 	//composante dérivée
-	commande+=(long int) KD_ALPHA*(erreur_alpha.actuelle-erreur_alpha.preced);
+	commande+=(long int) kp_alpha*(erreur_alpha.actuelle-erreur_alpha.preced);
 
 	return commande;
+}
+
+void set_kp_delta(int new_kp_delta)
+{
+    kp_delta=new_kp_delta;
+}
+
+void set_kd_delta(int new_kd_delta)
+{
+    kd_delta=new_kd_delta;
+}
+
+void set_kp_alpha(int new_kp_alpha)
+{
+    kp_alpha=new_kp_alpha;
+}
+
+void set_kd_alpha(int new_kd_alpha)
+{
+    kd_alpha=new_kd_alpha;
 }
--- PID.h
+++ PID.h
@@ -6,4 +6,9 @@
 long int PID_lineique(s_erreur erreur_delta);
 long int PID_angulaire(s_erreur erreur_alpha);
 
+void set_kp_delta(int new_kp_delta);
+void set_kd_delta(int new_kd_delta);
+void set_kp_alpha(int new_kp_alpha);
+void set_kd_alpha(int new_kd_alpha);
+
 #endif
