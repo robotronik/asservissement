f7fd96de3f8ed8fbf07c3e1007e3d8e7ef8a0e96
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2016-04-17 17:01:28 +0200

    ajoute une structure de position

--- maths_utils.h
+++ maths_utils.h
@@ -1,6 +1,17 @@
 #ifndef MATH_PRECALC_H
 #define MATH_PRECALC_H
 
+typedef struct {
+	//coordonées carthésiennes
+	float x;
+	float y;
+	//"coordonnées polaires"
+	long int delta;
+	long int alpha;
+	//orientation
+	int theta;
+} s_position;
+
 float cos_precalc(int angle);
 float sin_precalc(int angle);
 
