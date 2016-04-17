c946324cca1e76c26780a6671f7a745c958b528a
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2016-04-17 20:28:27 +0200

    rend possible l'utilisation d'une variable trajectoire externe

--- trajectoire.c
+++ trajectoire.c
@@ -11,20 +11,20 @@
 #include "reglages.h"
 #include "maths_utils.h"
 
-void update_consigne();
+void update_consigne(s_trajectoire * trajectoire);
 
-void make_trajectoire_alpha_delta(int new_alpha, int new_delta);
-void make_trajectoire_xy_relatif(int x_voulu, int y_voulu);
-void make_trajectoire_xy_absolu(int x_voulu, int y_voulu);
-void make_trajectoire_theta(int theta_voulu);
-void make_trajectoire_chemin(s_liste liste_positions);
+void process_trajectoire_alpha_delta(s_trajectoire * trajectoire);
+void process_trajectoire_xy_relatif(s_trajectoire * trajectoire);
+void process_trajectoire_xy_absolu(s_trajectoire * trajectoire);
+void process_trajectoire_theta(s_trajectoire * trajectoire);
+void process_trajectoire_chemin(s_trajectoire * trajectoire);
 void calcul_alpha_delta_restant(int x_voulu, int y_voulu, int * new_alpha, int * new_delta);
 
 void set_consigne_alpha_delta(int new_alpha, int new_delta);
 
 //et encore de vilaines variables globales !
-static s_trajectoire trajectoire;
- s_consigne consigne;
+static s_trajectoire trajectoire_loc;
+ s_position consigne;
 
 void start_asser()
 {
@@ -37,7 +37,8 @@ void start_asser()
 		actualise_position(position_actuelle);
 
 		//on met à jour la consigne pour l'asser
-		update_consigne();
+		s_trajectoire * trajectoire=get_trajectoire();
+		update_consigne(trajectoire);
 
 		//asservissement
 		s_position pos_consigne;
@@ -53,27 +54,27 @@ void start_asser()
 	}
 }
 
-void update_consigne(s_position consigne)
+void update_consigne(s_trajectoire * trajectoire)
 {
-	switch (trajectoire.type)
+	switch (trajectoire->type)
 	{
 		case alpha_delta :
-			make_trajectoire_alpha_delta(trajectoire.alpha,trajectoire.delta);
+			process_trajectoire_alpha_delta(trajectoire);
 			break;
 		case theta :
-			make_trajectoire_theta(trajectoire.theta);
+			process_trajectoire_theta(trajectoire);
 			break;
 		case xy_relatif :
-			make_trajectoire_xy_relatif(trajectoire.x_relatif,trajectoire.y_relatif);
+			process_trajectoire_xy_relatif(trajectoire);
 			break;
 		case xy_absolu :
-			make_trajectoire_xy_absolu(trajectoire.x_absolu,trajectoire.y_absolu);
+			process_trajectoire_xy_absolu(trajectoire);
 			break;
 		case chemin :
-			make_trajectoire_chemin(trajectoire.chemin);
+			process_trajectoire_chemin(trajectoire);
 			break;
 		case stop :
-			make_trajectoire_alpha_delta(0,0);
+			process_trajectoire_alpha_delta(trajectoire);
 			break;
 		case emergency_stop :
 			motors_stop(); //dans hardware_*.c
@@ -84,14 +85,17 @@ void update_consigne(s_position consigne)
 	}
 }
 
-void make_trajectoire_alpha_delta(int new_alpha, int new_delta)
+void process_trajectoire_alpha_delta(s_trajectoire * trajectoire)
 {
+	int new_alpha=trajectoire->alpha;
+	int new_delta=trajectoire->delta;
 	set_consigne_alpha_delta(new_alpha,new_delta);
-	trajectoire.type=null;
+	trajectoire->type=null;
 }
 
-void make_trajectoire_theta(int theta_voulu)
+void process_trajectoire_theta(s_trajectoire * trajectoire)
 {
+	int theta_voulu=trajectoire->theta;
 	int new_alpha=theta_voulu-get_theta_actuel();
 	//on borne *new_alpha entre pi et -pi
 	new_alpha=new_alpha%((int)(DEUX_PI*1000.0));
@@ -104,26 +108,30 @@ void make_trajectoire_theta(int theta_voulu)
 		new_alpha+=(int)(DEUX_PI*1000.0);
 	}
 	set_consigne_alpha_delta(new_alpha,0);
-	trajectoire.type=null;
+	trajectoire->type=null;
 }
 
-void make_trajectoire_xy_relatif(int x_voulu, int y_voulu)
+void process_trajectoire_xy_relatif(s_trajectoire * trajectoire)
 {
-	trajectoire.x_absolu=x_voulu+get_x_actuel();
-	trajectoire.y_absolu=y_voulu+get_y_actuel();
-	trajectoire.type=xy_absolu;
-	make_trajectoire_xy_absolu(trajectoire.x_absolu,trajectoire.y_absolu);
+	int x_voulu=trajectoire->x;
+	int y_voulu=trajectoire->y;
+	trajectoire->x=x_voulu+get_x_actuel();
+	trajectoire->y=y_voulu+get_y_actuel();
+	trajectoire->type=xy_absolu;
+	process_trajectoire_xy_absolu(trajectoire);
 }
 
-void make_trajectoire_xy_absolu(int x_voulu, int y_voulu)
+void process_trajectoire_xy_absolu(s_trajectoire * trajectoire)
 {
+	int x_voulu=trajectoire->x;
+	int y_voulu=trajectoire->y;
 	int new_alpha;
 	int new_delta;
 	x_voulu-=get_x_actuel();
 	y_voulu-=get_y_actuel();
 	calcul_alpha_delta_restant(x_voulu, y_voulu, &new_alpha, &new_delta);
 
-	if(trajectoire.mode==courbe)
+	if(trajectoire->mode==courbe)
 	{
 		//évite que le robot ne tourne pour rien quand il a atteint xy avec la précision voulue
 		if (new_delta<-PRECISION_DELTA || PRECISION_DELTA<new_delta)
@@ -132,10 +140,10 @@ void make_trajectoire_xy_absolu(int x_voulu, int y_voulu)
 		}
 		else
 		{
-			trajectoire.type=stop;
+			trajectoire->type=stop;
 		}
 	}
-	else if(trajectoire.mode==tendu)
+	else if(trajectoire->mode==tendu)
 	{
 		//on tourne pour se mettre dans la bonne direction
 		if (new_alpha<-PRECISION_ALPHA || PRECISION_ALPHA<new_alpha)
@@ -145,18 +153,19 @@ void make_trajectoire_xy_absolu(int x_voulu, int y_voulu)
 		//puis on avance tout droit (/!\ perte de précision)
 		else
 		{
-			trajectoire.type=alpha_delta;
-			trajectoire.delta=new_delta;
-			trajectoire.alpha=0;
+			trajectoire->type=alpha_delta;
+			trajectoire->delta=new_delta;
+			trajectoire->alpha=0;
 		}
 	}
 }
 
-void make_trajectoire_chemin(s_liste liste_positions)
+void process_trajectoire_chemin(s_trajectoire * trajectoire)
 {
-	if(trajectoire.chemin.taille==0)
+	s_liste liste_positions=trajectoire->chemin;
+	if(trajectoire->chemin.taille==0)
 	{
-		trajectoire.type=stop;
+		trajectoire->type=stop;
 		return;
 	}
 
@@ -172,14 +181,14 @@ void make_trajectoire_chemin(s_liste liste_positions)
 	if (new_delta<-PRECISION_DELTA || PRECISION_DELTA<new_delta)
 	{
 		//si il n'y a plus qu'un point la vitesse ne doit plus être constante, il faut ralentir
-		if(trajectoire.chemin.taille==1)
+		if(trajectoire->chemin.taille==1)
 			set_consigne_alpha_delta(new_alpha,new_delta);
 		else
 			set_consigne_alpha_delta(new_alpha,sgn_delta*CONSTANTE_DELTA);
 	}
 	else
 	{
-		trajectoire.chemin.taille-=1;
+		trajectoire->chemin.taille-=1;
 	}
 }
 
@@ -226,37 +235,37 @@ void calcul_alpha_delta_restant(int x_voulu, int y_voulu, int * new_alpha, int *
 	}
 }
 
-void set_trajectoire_alpha_delta(int alpha, int delta)
+void build_trajectoire_alpha_delta(s_trajectoire * trajectoire, int alpha, int delta)
 {
-	trajectoire.type=alpha_delta;
-	trajectoire.alpha=alpha;
-	trajectoire.delta=delta;
+	trajectoire->type=alpha_delta;
+	trajectoire->alpha=alpha;
+	trajectoire->delta=delta;
 }
 
-void set_trajectoire_xy_relatif(int x, int y)
+void build_trajectoire_xy_relatif(s_trajectoire * trajectoire, int x, int y)
 {
-	trajectoire.type=xy_relatif;
-	trajectoire.x_relatif=x;
-	trajectoire.y_relatif=y;
+	trajectoire->type=xy_relatif;
+	trajectoire->x=x;
+	trajectoire->y=y;
 }
 
-void set_trajectoire_xy_absolu(int x, int y)
+void build_trajectoire_xy_absolu(s_trajectoire * trajectoire, int x, int y)
 {
-	trajectoire.type=xy_absolu;
-	trajectoire.x_absolu=x;
-	trajectoire.y_absolu=y;
+	trajectoire->type=xy_absolu;
+	trajectoire->x=x;
+	trajectoire->y=y;
 }
 
-void set_trajectoire_theta(int new_theta)
+void build_trajectoire_theta(s_trajectoire * trajectoire, int new_theta)
 {
-	trajectoire.type=theta;
-	trajectoire.theta=new_theta;
+	trajectoire->type=theta;
+	trajectoire->theta=new_theta;
 }
 
-void set_trajectoire_chemin(s_liste liste_positions)
+void build_trajectoire_chemin(s_trajectoire * trajectoire, s_liste liste_positions)
 {
-	trajectoire.type=chemin;
-	trajectoire.chemin=liste_positions;
+	trajectoire->type=chemin;
+	trajectoire->chemin=liste_positions;
 }
 
 void set_consigne_alpha_delta(int new_alpha, int new_delta)
@@ -265,40 +274,90 @@ void set_consigne_alpha_delta(int new_alpha, int new_delta)
 	consigne.delta=new_delta+get_delta_actuel();
 }
 
+void build_trajectoire_stop(s_trajectoire * trajectoire)
+{
+	trajectoire->type=stop;
+	build_trajectoire_alpha_delta(trajectoire,0,0);
+}
+
+void build_trajectoire_emergency_stop(s_trajectoire * trajectoire)
+{
+	trajectoire->type=emergency_stop;
+}
+
+// TODO : renomer
+void set_trajectoire_mode2(s_trajectoire * trajectoire, e_mode_deplacement mode)
+{
+	trajectoire->mode=mode;
+}
+
+//TODO : renomer
+void init_trajectoire2(s_trajectoire * trajectoire)
+{
+	trajectoire->type=null;
+	set_consigne_alpha_delta(0,0);
+	set_trajectoire_mode2(trajectoire, MODE_TRAJECTOIRE);
+}
+
+//TODO : remove
+void init_trajectoire()
+{
+	trajectoire_loc.type=null;
+	set_consigne_alpha_delta(0,0);
+	set_trajectoire_mode2(&trajectoire_loc, MODE_TRAJECTOIRE);
+}
+
+s_trajectoire * get_trajectoire()
+{
+	return &trajectoire_loc;
+}
+
+//TODO : remove
+void set_trajectoire_alpha_delta(int alpha, int delta)
+{
+	trajectoire_loc.type=alpha_delta;
+	trajectoire_loc.alpha=alpha;
+	trajectoire_loc.delta=delta;
+}
+
+void set_trajectoire_xy_relatif(int x, int y)
+{
+	trajectoire_loc.type=xy_relatif;
+	trajectoire_loc.x=x;
+	trajectoire_loc.y=y;
+}
+
+void set_trajectoire_xy_absolu(int x, int y)
+{
+	trajectoire_loc.type=xy_absolu;
+	trajectoire_loc.x=x;
+	trajectoire_loc.y=y;
+}
+
+void set_trajectoire_theta(int new_theta)
+{
+	trajectoire_loc.type=theta;
+	trajectoire_loc.theta=new_theta;
+}
+
+void set_trajectoire_chemin(s_liste liste_positions)
+{
+	trajectoire_loc.type=chemin;
+	trajectoire_loc.chemin=liste_positions;
+}
+
 void set_trajectoire_stop()
 {
-	trajectoire.type=stop;
+	trajectoire_loc.type=stop;
+	set_trajectoire_alpha_delta(0,0);
 }
 
 void set_trajectoire_emergency_stop()
 {
-	trajectoire.type=emergency_stop;
+	trajectoire_loc.type=emergency_stop;
 }
 
 void set_trajectoire_mode(e_mode_deplacement mode)
 {
-	trajectoire.mode=mode;
-}
-
-void init_trajectoire()
-{
-	trajectoire.type=null;
-	set_consigne_alpha_delta(0,0);
-	set_trajectoire_mode(MODE_TRAJECTOIRE);
-}
-
-int trajectoire_type_is_null()
-{
-	return (trajectoire.type==null);
-}
-
-//TODO : à virer (utile uniquement pour du debug)
-int get_delta_voulu()
-{
-	return consigne.delta;
-}
-
-int get_alpha_voulu()
-{
-	return consigne.alpha;
+	trajectoire_loc.mode=mode;
 }
--- trajectoire.h
+++ trajectoire.h
@@ -41,39 +41,41 @@ typedef struct
 	int alpha;
 	int delta;
 	int theta;
-	int x_absolu;
-	int y_absolu;
-	int x_relatif;
-	int y_relatif;
+	int x;
+	int y;
 	s_liste chemin;
 
 }s_trajectoire;
-
-typedef struct
-{
-	long int alpha;
-	long int delta;
-} s_consigne;
 
 
 void init_trajectoire();
 void start_asser();
 
 //appelé par la stratégie
+void build_trajectoire_alpha_delta(s_trajectoire * trajectoire, int alpha, int delta);
+void build_trajectoire_xy_relatif(s_trajectoire * trajectoire, int x, int y);
+void build_trajectoire_xy_absolu(s_trajectoire * trajectoire, int x, int y);
+void build_trajectoire_theta(s_trajectoire * trajectoire, int new_theta);
+void build_trajectoire_chemin(s_trajectoire * trajectoire, s_liste liste_positions);
+void build_trajectoire_stop(s_trajectoire * trajectoire);
+void build_trajectoire_emergency_stop(s_trajectoire * trajectoire); //stop définitif et violent (fin de match)
+//TODO : renomer
+void set_trajectoire_mode2(s_trajectoire * trajectoire, e_mode_deplacement mode);
+
+//appelé dans l'asser pour send_position_atteinte
+int trajectoire_type_is_null();
+
+s_trajectoire * get_trajectoire();
+
+//TODO : remove
 void set_trajectoire_alpha_delta(int alpha, int delta);
 void set_trajectoire_xy_relatif(int x, int y);
 void set_trajectoire_xy_absolu(int x, int y);
 void set_trajectoire_theta(int new_theta);
 void set_trajectoire_chemin(s_liste liste_positions);
 void set_trajectoire_stop();
-void set_trajectoire_emergency_stop(); //stop définitif et violent (fin de match)
+void set_trajectoire_emergency_stop();
 void set_trajectoire_mode(e_mode_deplacement mode);
 
-//appelé dans l'asser pour send_position_atteinte
-int trajectoire_type_is_null();
-
-//utile uniquement pour du debug
-int get_delta_voulu();
-int get_alpha_voulu();
 
 #endif
